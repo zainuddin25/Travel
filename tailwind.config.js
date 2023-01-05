19e5338ee7f5ed5ca7bfd7e139f75984d7d50057
zainuddin25 <zanuk12345>
2023-01-05 11:07:56 +0700

    add: header page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'white-bone' : '#f6f2ea'
+      },
+      fontFamily: {
+        'poppins' : ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
